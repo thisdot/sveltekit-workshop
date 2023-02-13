afd975c961b7680e008f4d0ca90b7b7722395d32
Adam L Barrett <adamlbarrett@hotmail.com>
2023-02-13 16:57:41 -0600

    add demo for module-6.2 Assets and Images

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import '../styles.css'
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<style>
+main {
+  padding: 0 3rem 3rem;
+}
+</style>
+
