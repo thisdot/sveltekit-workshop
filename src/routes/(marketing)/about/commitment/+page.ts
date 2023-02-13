05df7fb81f2c6cdded6899db3f824d5e7b127cf3
Adam L Barrett <adamlbarrett@hotmail.com>
2023-02-13 01:19:24 -0600

    setup module 3.3

--- src/routes/(marketing)/about/commitment/+page.ts
+++
@@ -1,6 +0,0 @@
-import { error } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
-
-export const load = (async () => {
-	throw error(404, 'There is a lack of commitment here');
-}) satisfies PageLoad;
