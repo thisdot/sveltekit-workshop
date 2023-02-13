0497938ee57b7a1e909a6575e85920eab72f40b6
Adam L Barrett <adamlbarrett@hotmail.com>
2023-02-13 16:48:18 -0600

    Add solution for module-5.0 Forms

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
 
 type Movie = {
 	id: string;
@@ -8,7 +8,7 @@ type Movie = {
 	comment?: string;
 };
 
-const movies: Movie[] = [
+let movies: Movie[] = [
 	{
 		id: 'df5105ff-9d87-4bd9-be62-4743c0a243b9',
 		name: 'Everything Everywhere All at Once',
@@ -25,8 +25,80 @@ const movies: Movie[] = [
 	}
 ];
 
-export const load: PageServerLoad = () => {
+export const load = () => {
 	return {
 		movies
 	};
+};
+
+const validateMovie = (movieData: Omit<Movie, 'id'>) => {
+	if (!movieData.name) {
+		return {
+			success: false,
+			error: `Name of the movie is required`
+		};
+	}
+	if (!movieData.release) {
+		return {
+			success: false,
+			error: `Release Year is required`
+		};
+	}
+	if (!movieData.rating) {
+		return {
+			success: false,
+			error: `Please rate the movie from 1 to 5 stars`
+		};
+	}
+	return {
+		success: true
+	};
+};
+
+export const actions = {
+	async logMovie({ request }) {
+		const formData = await request.formData();
+
+		// await new Promise((res) => setTimeout(res, 3000));
+
+		const name = formData.get('name');
+		const release = formData.get('release');
+		const rating = formData.get('rating');
+		const comment = formData.get('comment');
+
+		if (name === 'Adam') {
+			throw redirect(303, 'super-secret-url');
+		}
+
+		const movieData: Omit<Movie, 'id'> = {
+			name: typeof name === 'string' ? name : '',
+			release: Number(release),
+			rating: Number(rating),
+			comment: typeof comment === 'string' ? comment : ''
+		};
+
+		const validation = validateMovie(movieData);
+
+		if (validation.success) {
+			movies.push({ id: crypto.randomUUID(), ...movieData });
+		} else {
+			return fail(400, {
+				name,
+				release,
+				rating,
+				comment,
+				errorMessage: validation.error
+			});
+		}
+
+		return {
+			success: true
+		};
+	},
+
+	async deleteMovie({ request }) {
+		const movieId = (await request.formData()).get('movieToDelete');
+		console.log('DELETE', movieId);
+		movies = movies.filter((m) => m.id !== movieId);
+	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,84 @@
 <script lang="ts">
-  import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
 
-  export let data: PageData;
+	export let data;
+	export let form;
 
-  $: movies = data?.movies ?? [];
+	$: movies = data?.movies ?? [];
+
+	let loading = false;
 </script>
 
 <svelte:head>
-  <title>Home</title>
+	<title>Home</title>
 </svelte:head>
 
 <h1>Movie Log</h1>
 
+<!-- <form use:enhance={() => {
+          console.log('BEFORE ACTIONS')
+          loading = true;
+          return ({ result, update }) => {
+            console.log('AFTER ACTION', result)
+            loading = false;
+            update()
+          }  
+        }} method="post" action="?/logMovie"> -->
+<form use:enhance method="post" action="?/logMovie">
+	<label for="name">Name</label>
+	<input id="name" name="name" value={form?.name ?? null} />
+
+	<label for="release">Release</label>
+	<input type="number" id="release" name="release" value={form?.release ?? null} />
+
+	<label for="rating">Rating</label>
+	<select id="rating" name="rating" value={form?.rating ?? ''}>
+		<option>Select a rating</option>
+		<option value="1">1 Stars ⭐️</option>
+		<option value="2">2 Stars ⭐️⭐️</option>
+		<option value="3">3 Stars ⭐️⭐️⭐️</option>
+		<option value="4">4 Stars ⭐️⭐️⭐️⭐️</option>
+		<option value="5">5 Stars ⭐️⭐️⭐️⭐️⭐️</option>
+	</select>
+
+	<label for="comment">Comment:</label>
+	<textarea
+		id="comment"
+		name="comment"
+		placeholder="What did you think?"
+		value={typeof form?.comment === 'string' ? form.comment : ''}
+	/>
+
+	<div class="submission">
+		<span class="error">{form?.errorMessage ?? ''}</span>
+		<button type="submit">
+			{loading ? '...loading' : 'Log The Movie'}
+		</button>
+	</div>
+</form>
+
 <table>
-  <thead>
-    <tr>
-      <th class="name">name</th>
-      <th class="release">release</th>
-      <th class="rating">rating</th>
-      <th class="comment">comment</th>
-    </tr>
-  </thead>
-  <tbody>
-  {#each movies as movie}
-    <tr>
-      <td class="name">{movie.name}</td>
-      <td class="release">{movie.release}</td>
-      <td class="rating">{movie.rating}</td>
-      <td class="comment">{movie.comment}</td>
-  {/each}
-  <tbody>
+	<thead>
+		<tr>
+			<th class="name">name</th>
+			<th class="release">release</th>
+			<th class="rating">rating</th>
+			<th class="comment">comment</th>
+			<th class="action" />
+		</tr>
+	</thead>
+	<tbody>
+		{#each movies as movie}
+			<tr>
+				<td class="name">{movie.name}</td>
+				<td class="release">{movie.release}</td>
+				<td class="rating">{movie.rating}</td>
+				<td class="comment">{movie.comment}</td>
+				<td class="action">
+					<form use:enhance method="post" action="?/deleteMovie">
+						<button type="submit" name="movieToDelete" value={movie.id}>Delete</button>
+					</form>
+				</td>
+			</tr>{/each}
+	</tbody><tbody />
 </table>
