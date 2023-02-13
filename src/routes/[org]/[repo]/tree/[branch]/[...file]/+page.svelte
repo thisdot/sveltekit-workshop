8da8860d1bcdad0846d5815dd5fa68233e338eb1
Adam L Barrett <adamlbarrett@hotmail.com>
2023-02-13 01:08:48 -0600

    Adding demo for module-3.1

---
+++ src/routes/[org]/[repo]/tree/[branch]/[...file]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<code>
+	{JSON.stringify(data?.params, null, 2)}
+</code>
+
+<style>
+	code {
+		white-space: pre-wrap;
+	}
+</style>
